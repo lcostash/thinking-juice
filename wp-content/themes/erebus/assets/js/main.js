2b1f25722d47dab54505035afd1c104739ad7f50
Lilian Costas <lcostash@gmail.com>
2020-11-10 17:14:01 +0000

    finish implement the menu

--- wp-content/themes/erebus/assets/js/main.js
+++ wp-content/themes/erebus/assets/js/main.js
@@ -8,7 +8,16 @@
 
     ThinkingJuice.prototype.init = function () {
         this.$el.find('.site-header__menu').off('click').on('click', function () {
-            $(this).toggleClass('site-header__menu--change');
+            const body = $(this).closest('body');
+            $(body).find('.site-header__menu').addClass('site-header__menu--change');
+            $(body).addClass('modal-form');
+            $(body).find('.site-menu').addClass('site-menu--display');
+        });
+        this.$el.find('.site-menu__close').off('click').on('click', function () {
+            const body = $(this).closest('body');
+            $(body).find('.site-header__menu').removeClass('site-header__menu--change');
+            $(body).removeClass('modal-form');
+            $(body).find('.site-menu').removeClass('site-menu--display');
         });
     };
 
