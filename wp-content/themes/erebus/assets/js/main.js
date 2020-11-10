60bb24f1c94cbc87b4f521108a319a6a3b6afc2d
Lilian Costas <lcostash@gmail.com>
2020-11-10 15:48:53 +0000

    finish create front-page

--- wp-content/themes/erebus/assets/js/main.js
+++ wp-content/themes/erebus/assets/js/main.js
@@ -1,3 +1,25 @@
-(function ($) {
++function ($) {
+    'use strict';
 
-})(jQuery);
+    var ThinkingJuice = function (element) {
+        this.$el = $(element);
+        this.init();
+    };
+
+    ThinkingJuice.prototype.init = function () {
+        this.$el.find('.site-header__menu').off('click').on('click', function () {
+            $(this).toggleClass('site-header__menu--change');
+        });
+    };
+
+    $.fn.thinkingjuice = function () {
+        return $(this).each(function () {
+            new ThinkingJuice(this);
+        });
+    };
+    $.fn.thinkingjuice.Constructor = ThinkingJuice;
+
+    $(document).ready(function () {
+        $('body').thinkingjuice();
+    });
+}(window.jQuery);
